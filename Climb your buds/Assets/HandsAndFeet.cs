4c5a7b9a1463bb2aac96cf4c034f2a23c60377ca
BrianCraddock <BrianLCraddock@gmail.com>
2015-11-14 20:45:01 -0500

    working limbs

---
+++ Climb your buds/Assets/HandsAndFeet.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandsAndFeet : MonoBehaviour {
+	public bool isActive = false;	
+	public bool isStuck = false;	
+	public float movementSpeed = 5;  
+	public string buttonKey;
+
+	void Start () {
+		foreach (Transform child in transform.parent) {
+			if (child.GetComponent<Collider2D>() != null) {
+				Physics2D.IgnoreCollision(child.GetComponent<Collider2D>(),GetComponent<Collider2D>());
+			}
+		}
+	}
+
+	void FixedUpdate () {
+
+		if (Input.GetKey (buttonKey)) {
+			isActive = true;
+			isStuck = false;
+			GetComponent<SpriteRenderer> ().color = Color.green;
+		} else {		
+			isActive = false;
+			GetComponent<SpriteRenderer> ().color = Color.red;
+		}
+
+		if (!isStuck) {			
+			GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+			float h = Input.GetAxisRaw("Horizontal");
+			float v = Input.GetAxisRaw("Vertical");		
+			GetComponent<Rigidbody2D>().velocity = new Vector2(h, v) * movementSpeed;
+		}
+	}
+
+	
+	void OnCollisionEnter2D(Collision2D collision) {
+		isStuck = true;
+		if (!isActive) {
+			isStuck = true;
+			GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
+		}
+		Debug.Log("Collision");
+	}
+}
--- Climb your buds/Assets/Player.cs
+++ Climb your buds/Assets/Player.cs
@@ -2,8 +2,7 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
-	
-	public float movementSpeed = 5;  
+
 	public bool frozen = false;
 	// Use this for initialization
 	void Start () {
@@ -26,19 +25,16 @@ public class Player : MonoBehaviour {
 	}
 
 	private void Movement () {		
-		float h = Input.GetAxisRaw("Horizontal");
-		float v = Input.GetAxisRaw("Vertical");		
+
 		var camera = GameObject.Find ("Main Camera");
 		var playerPosition = GetComponent<Rigidbody2D> ().position;
 
-		GetComponent<Rigidbody2D>().velocity = new Vector2(h, v) * movementSpeed;
 		camera.GetComponent<Transform> ().position = new Vector3(playerPosition.x, playerPosition.y, -10) ;
 	}
 
 	private void Freeze() {
 		var childrenJoints = GetComponentsInChildren<DistanceJoint2D> ();
 		var childrenRigidBodies = GetComponentsInChildren<Rigidbody2D> ();
-		var rigidBody = GetComponent<Rigidbody2D> ();
 
 		foreach(DistanceJoint2D joint in childrenJoints) {
 			Destroy(joint);
